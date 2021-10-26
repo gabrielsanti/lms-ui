b7cbc338b2cfbfe97bdca157e5c559e004348335
Gabriel Santillan <gabriel.santillan@amicucciformazione.com>
2021-10-26 14:29:12 +0200

    Initial layout

---
+++ src/containers/courses/courses.jsx
@@ -0,0 +1,211 @@
+import React from 'react';
+import Logo from '../../images/headerlogo.png';
+
+function Courses() {
+  return (
+    <div>
+
+      <nav className=" bg-white w-full flex relative justify-between items-center mx-auto px-8 h-20 shadow-md">
+
+        <div className="inline-flex">
+          <a className="_o6689fn" href="/"
+          >
+            <img src={Logo} alt="SkillaHub" style={{ maxWidth: "10rem" }} />
+          </a>
+        </div>
+
+        {/* end logo */}
+
+        {/* search bar */}
+        <div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
+          <div className="inline-block">
+            <div className="inline-flex items-center max-w-full">
+              <button className="flex items-center flex-grow-0 flex-shrink pl-2 relative w-60 border rounded-full px-1  py-1" type="button">
+                <div className="block flex-grow flex-shrink overflow-hidden">Start your search</div>
+                <div className="flex items-center justify-center relative  h-8 w-8 rounded-full">
+                  <svg
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    role="presentation"
+                    focusable="false"
+                    style={{
+                      display: "block",
+                      fill: "none",
+                      height: "12px",
+                      width: "12px",
+                      stroke: "currentcolor",
+                      strokeWidth: "5.33333",
+                      overflow: "visible"
+
+                    }}
+                  >
+                    <g fill="none">
+                      <path
+                        d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+                      ></path>
+                    </g>
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* end search bar */}
+
+        {/* login */}
+        <div className="flex-initial">
+          <div className="flex justify-end items-center relative">
+            <div className="flex mr-4 items-center">
+
+              <div className="block relative">
+                <button type="button" className="inline-block py-2 px-3 hover:bg-gray-200 rounded-full relative ">
+                  <div className="flex items-center h-5">
+                    <div className="_xpkakx">
+                      <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", height: "16px", width: "16px", fill: "currentcolor" }}><path d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"></path></svg>
+                    </div>
+                  </div>
+                </button>
+              </div>
+            </div>
+
+            <div className="block">
+              <div className="inline relative">
+                <button type="button" className="inline-flex items-center relative px-2 border rounded-full hover:shadow-lg">
+                  <div className="pl-1">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style={{ display: "block", fill: "none", height: "16px", width: "16px", stroke: "currentcolor", strokeWidth: 3, overflow: "visible" }}
+                    >
+                      <g fill="none" fill-rule="nonzero">
+                        <path d="m2 16h28"></path>
+                        <path d="m2 24h28"></path>
+                        <path d="m2 8h28"></path>
+                      </g>
+                    </svg>
+                  </div>
+
+                  <div className="block flex-grow-0 flex-shrink-0 h-10 w-12 pl-5">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style={{ display: "block", height: "100%", width: "100%", fill: "currentcolor" }}
+                    >
+                      <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+                    </svg>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end login */}
+      </nav>
+      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mt-12 mb-12">
+        <article>
+          <h2 className="text-2xl font-extrabold text-gray-900">OUR COURSES</h2>
+          <section className="mt-6 grid md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8">
+            <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+              <div className="relative w-full h-80 md:h-64 lg:h-44">
+                <img src="https://cdn.pixabay.com/photo/2021/07/24/01/42/zebra-dove-6488440_960_720.jpg"
+                  alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                  className="w-full h-full object-center object-cover" />
+              </div>
+              <div className="px-3 py-4">
+
+                <h3 className="text-sm text-gray-500 pb-2">
+                  <a className="bg-yellow-600 py-1 px-2 text-white rounded-lg" href="/course/1">
+                    <span className="absolute inset-0"></span>
+                    Categoria 4
+                  </a>
+                </h3>
+                <div className="relative pt-1">
+                  <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200">
+                    <div style={{ width: "30%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"></div>
+                  </div>
+                </div>
+                <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>
+            </article>
+            <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+              <div className="relative w-full h-80 md:h-64 lg:h-44">
+                <img src="https://cdn.pixabay.com/photo/2021/09/08/20/45/bird-6607863_960_720.jpg.jpg"
+                  alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                  className="w-full h-full object-center object-cover" />
+              </div>
+              <div className="px-3 py-4">
+                <h3 className="text-sm text-gray-500 pb-2">
+                  <a className="bg-green-600 py-1 px-2 text-white rounded-lg" href="/course/2">
+                    <span className="absolute inset-0"></span>
+                    Categoria 1
+                  </a>
+                </h3>
+                <div className="relative pt-1">
+                  <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200">
+                    <div style={{ width: "90%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"></div>
+                  </div>
+                </div>
+                <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>
+            </article>
+            <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+              <div className="relative w-full h-80 md:h-64 lg:h-44">
+                <img src="https://cdn.pixabay.com/photo/2021/08/03/11/01/stairs-6519085_960_720.jpg"
+                  alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                  className="w-full h-full object-center object-cover" />
+              </div>
+              <div className="px-3 py-4">
+                <h3 className="text-sm text-gray-500 pb-2">
+                  <a className="bg-red-600 py-1 px-2 text-white rounded-lg" href="/course/3">
+                    <span className="absolute inset-0"></span>
+                    Categoria 3
+                  </a>
+                </h3>
+                <div className="relative pt-1">
+                  <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200">
+                    <div style={{ width: 0 }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"></div>
+                  </div>
+                </div>
+                <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>
+            </article>
+            <article className="bg-white group relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform duration-200">
+              <div className="relative w-full h-80 md:h-64 lg:h-44">
+                <img src="https://cdn.pixabay.com/photo/2021/09/12/17/43/parrot-feathers-6619082_960_720.jpg"
+                  alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                  className="w-full h-full object-center object-cover" />
+              </div>
+              <div className="px-3 py-4">
+                <h3 className="text-sm text-gray-500 pb-2">
+                  <a className="bg-blue-600 py-1 px-2 text-white rounded-lg" href="/course/4">
+                    <span className="absolute inset-0"></span>
+                    Categoria 2
+                  </a>
+                </h3>
+                <div className="relative pt-1">
+                  <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-indigo-200">
+                    <div style={{ width: 0 }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"></div>
+                  </div>
+                </div>
+                <p className="text-base font-semibold text-gray-900 group-hover:text-indigo-600">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>
+            </article>
+          </section>
+        </article>
+      </section>
+    </div>
+  )
+};
+
+export default Courses;
