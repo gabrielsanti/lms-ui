bde3ad28e75814dbcf7edb0ef100e4df6e371095
Gabriel Santillan <gabrielsanti@gmail.com>
2021-11-02 19:51:53 +0100

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-/*import Login from "./containers/login/login";*/
+import Login from "./containers/login/login"
 import Courses from "./containers/courses/courses"
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      {/* <Login /> */}
-      <Courses />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/mycourses">
+            <Courses />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,7 +1,7 @@
-// @ts-check
-
 import React from "react";
 import headerLogo from "../../images/headerlogo.png"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
 function Login() {
   return (
 
@@ -19,7 +19,10 @@ function Login() {
               <input type="password" className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded" placeholder="Your password" />
             </div>
             <span className="text-sm text-gray-700 inline-block mt-4 hover:text-indigo-600 hover:underline hover:cursor-pointer transition duration-200">Forgot password</span>
-            <button className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300">LOGIN</button>
+            <Link to="/mycourses">
+              <button className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300" >Login</button>
+              </Link>
+
           </div>
         </div>
       </div>
